28f756a130fa247f9fa9f69b93b7a5e26c426c91
Patrick <patrickpmqs@gmail.com>
2026-04-22 14:20:48 -0300

    rotas dos botoes na homepage

--- reserva-de-mesa/src/App.tsx
+++ reserva-de-mesa/src/App.tsx
@@ -1,20 +1,25 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import HomePage from "./Pages/homepage"
-import Reservas from "./Pages/reservas/reservas"
-import { NotificationCenter, NotificationProvider } from "./features/notifications"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/homepage";
+import Reservas from "./Pages/reservas/reservas";
+import {
+  NotificationCenter,
+  NotificationProvider,
+} from "./features/notifications";
+import Avaliacoes from "./Pages/avaliacoes";
 
 function App() {
-	return (
-		<NotificationProvider>
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<HomePage />} />
-					<Route path="/reservas" element={<Reservas />} />
-				</Routes>
-				<NotificationCenter />
-			</BrowserRouter>
-		</NotificationProvider>
-	)
+  return (
+    <NotificationProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/reservas" element={<Reservas />} />
+          <Route path="/avaliacoes" element={<Avaliacoes />} />
+        </Routes>
+        <NotificationCenter />
+      </BrowserRouter>
+    </NotificationProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ reserva-de-mesa/src/Pages/avaliacoes.tsx
@@ -0,0 +1,26 @@
+export default function Avaliacoes() {
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        width: "100vw",
+        background: "#102542",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <h1
+        style={{
+          color: "#fff",
+          fontSize: 36,
+          fontWeight: 600,
+          letterSpacing: 2,
+          textAlign: "center",
+        }}
+      >
+        Em breve
+      </h1>
+    </div>
+  );
+}
